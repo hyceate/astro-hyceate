dfc2fed46e26db42251050ba15b5cb7dcdd04e30
hyceate <hyceate+github@gmail.com>
2024-03-18 10:10:57 -0400

    client update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { loadEnv } from "vite";
-const {
-  PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET,
-  SANITY_PROJECT_ID, 
-  SANITY_DATASET, SANITY_API_VERSION, PUBLIC_SANITY_API_VERSION
-} = loadEnv(import.meta.env.MODE, process.cwd(), "");
+// import { loadEnv } from "vite";
+// const {
+//   PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET,
+//   SANITY_PROJECT_ID, 
+//   SANITY_DATASET, SANITY_API_VERSION, PUBLIC_SANITY_API_VERSION
+// } = loadEnv(import.meta.env.MODE, process.cwd(), "");
+// const projectId =  PUBLIC_SANITY_PROJECT_ID || SANITY_PROJECT_ID;
+// const dataset =  PUBLIC_SANITY_DATASET || SANITY_DATASET;
+// const apiVersion = PUBLIC_SANITY_API_VERSION || SANITY_API_VERSION;
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
@@ -11,16 +14,17 @@ import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import { vite as vidstack } from 'vidstack/plugins';
 import sanity from "@sanity/astro";
-const projectId =  PUBLIC_SANITY_PROJECT_ID || SANITY_PROJECT_ID;
-const dataset =  PUBLIC_SANITY_DATASET || SANITY_DATASET;
-const apiVersion = PUBLIC_SANITY_API_VERSION || SANITY_API_VERSION;
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), tailwind(), mdx(), sanity({
-  projectId,
-    dataset,
-    useCdn: true,
-    apiVersion,
+    projectId: 'u6uatr6p',
+  dataset: 'production',
+  useCdn: true, // set to `false` to bypass the edge cache
+  apiVersion: '2023-05-03', // use current date (YYYY-MM-DD) to target the latest API version
+  // projectId,
+  //   dataset,
+  //   useCdn: true,
+  //   apiVersion,
 }
   )],
   output: "hybrid",
