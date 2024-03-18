fa6221f2ab1ec0fde21cc998d8f8f1841e546045
hyceate <hyceate+github@gmail.com>
2024-03-18 11:29:22 -0400

    path?

--- src/lib/components/content/live2dPosts.svelte
+++ src/lib/components/content/live2dPosts.svelte
@@ -9,7 +9,7 @@
   id="live2d-card"
   class="flex flex-[1_1_15%] w-full px-1 py-1 m-2 bg-primary border-2 border-rose-200 rounded-lg shadow-[2px_2px_6px_#e0dad2,-2px_-4px_9px_#fffffe] hover:shadow-[1px_1px_1px_#e0dad2,-1px_-1px_2px_#fffffe] hover:scale-105 transition-transform ease-in-out motion-reduce:transition-none motion-reduce:hover:transform-none"
 >
-  <a href="{category}/{post.slug}">
+  <a href="/{category}/{post.slug}">
     <figure class="max-h-[15rem] overflow-hidden">
       <slot name="image" />
       <!-- <img
