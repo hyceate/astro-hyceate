e301959ad4bd8db6f4def0d49e8929045c473e6d
hyceate <hyceate+github@gmail.com>
2024-03-19 17:40:48 -0400

    finessing

--- src/assets/js/transition.js
+++ src/assets/js/transition.js
@@ -1,11 +1,10 @@
-import Swup from "@swup/astro";
+import Swup from "swup";
 import SwupA11yPlugin from "@swup/a11y-plugin";
 import SwupHeadPlugin from "@swup/head-plugin";
 import { isMenuOpen } from "@lib/stores/stores";
 const swup = new Swup({
   linkToSelf: 'navigate',
   plugins: [new SwupA11yPlugin(), new SwupHeadPlugin(), 
-    new SwupMorphPlugin({containers: []}), 
   ],
 });
 swup.hooks.on('animation:out:start', (visit) => {
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,5 @@
 /// <reference types="astro/client" />
 /// <reference types="@sanity/astro/module" />
+/// <reference types="astro-imagetools" />
+
+declare module "astro-imagetools/components"
