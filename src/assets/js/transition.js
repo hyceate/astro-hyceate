53a0c7156e9f42dae2b2d93f063f5b29d60b44d7
hyceate <hyceate+github@gmail.com>
2024-03-20 16:41:11 -0400

    loader

--- src/assets/js/transition.js
+++ src/assets/js/transition.js
@@ -1,17 +1,31 @@
 import Swup from "swup";
 import SwupA11yPlugin from "@swup/a11y-plugin";
 import SwupHeadPlugin from "@swup/head-plugin";
-import { isMenuOpen } from "@lib/stores/stores";
+import { isMenuOpen, isLoading } from "@lib/stores/stores";
 const swup = new Swup({
   linkToSelf: 'navigate',
   plugins: [new SwupA11yPlugin(), new SwupHeadPlugin(), 
   ],
+  resolveUrl: (url) => {
+    if (url.startsWith('/projects/')) {
+      return '/projects/';
+    }
+    return url;
+  },
 });
-// swup.hooks.on('animation:out:start', (visit) => {
-//   isMenuOpen.set(false);
-//   console.log('animation:out:start: ', visit.to.url);
-// });
-swup.hooks.on('visit:start', (visit) => {
+swup.hooks.on('link:click', (visit) => {
   isMenuOpen.set(false);
-  console.log('visit:start: ', visit.to.url);
+  isLoading.set(false);
+});
+swup.hooks.on('animation:out:start', () => {
+  document.querySelector('#loader').hidden = false;
+  document.querySelector('#loader-bg').hidden = false;
+  document.querySelector('#loader-svg').hidden = false;
+  isLoading.set(true);
+});
+swup.hooks.on('animation:in:end', () => {
+  document.querySelector('#loader').hidden = true;
+  document.querySelector('#loader-bg').hidden = true;
+  document.querySelector('#loader-svg').hidden = true;
+  isLoading.set(false);
 });
