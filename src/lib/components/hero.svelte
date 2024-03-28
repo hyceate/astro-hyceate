9b439ff9a57fca56f238706cca410e1dd9f64627
hyceate <hyceate+github@gmail.com>
2024-03-28 16:53:37 -0400

    lottie player

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -1,15 +1,27 @@
 <script lang="ts">
-	import landing from "@assets/svg/landing_1.svg";
+	import { LottiePlayer } from "@lottiefiles/svelte-lottie-player";
+	import Landing from "@assets/animation/landing.json";
+	// import landing from "@assets/svg/landing_1.svg";
 </script>
 
 <div
 	id="hero"
 	class="flex flex-col justify-center items-center m-auto overflow-clip"
 >
-	<img
+	<div class="max-w-[35rem]">
+		<LottiePlayer
+			src={Landing}
+			autoplay={true}
+			loop={true}
+			background="transparent"
+			renderer="svg"
+			controls={false}
+		/>
+	</div>
+	<!-- <img
 		class="w-full max-w-[35rem] transform"
 		src={landing.src}
 		alt="Hyceate at a desk with a graphics tablet"
-	/>
+	/> -->
 	<h1 class="text-9xl max-md:text-7xl text-[#db7471]">hyceate</h1>
 </div>
