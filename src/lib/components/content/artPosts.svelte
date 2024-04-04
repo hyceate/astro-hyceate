cad649b074a60e93b47c492b41a3c32acb1c453f
hyceate <hyceate+github@gmail.com>
2024-04-04 17:40:56 -0400

    inline modal instead of component

--- src/lib/components/content/artPosts.svelte
+++ src/lib/components/content/artPosts.svelte
@@ -5,7 +5,7 @@
   import { showModal } from "@lib/stores/stores";
   import { fetchList } from "@lib/stores/fetchData";
   import { onMount, onDestroy } from "svelte";
-  import ArtModal from "@components/content/art-modal.svelte";
+  // import ArtModal from "@components/content/art-modal.svelte";
   const category = "art";
 
   // console.log(`Post Data for art Cards`, post);
@@ -28,6 +28,7 @@
   }
 
   function closeModal() {
+    $showModal = false;
     modalImageUrl = null;
     modalAltText = null;
     history.pushState({ showModal: false }, "", `/projects/${category}`);
@@ -63,11 +64,6 @@
   });
 </script>
 
-<ArtModal
-  imageUrl={modalImageUrl}
-  altText={modalAltText}
-  on:close={closeModal}
-/>
 {#each posts as post}
   <li
     class="art-card flex-[1_1_20%] self-start w-full rounded-lg transition-all hover:scale-[102%] transition-transform ease motion-reduce:transition-none motion-reduce:hover:transform-none"
@@ -101,3 +97,84 @@
     </a>
   </li>
 {/each}
+<!-- MODAL -->
+<div
+  class="modal {$showModal ? 'isLoaded' : ''}"
+  on:click={closeModal}
+  aria-hidden="true"
+>
+  <button
+    class="close major-button absolute h-10 w-10 shadow z-[4000] top-0 right-50"
+    title="Close"
+    on:click={closeModal}
+    aria-label="Close">&times;</button
+  >
+  <div id="modal-image" class="max-w-full max-h-full">
+    <img
+      class="p-5 {$showModal ? 'isLoaded' : ''} max-w-full max-h-full"
+      src={modalImageUrl}
+      alt={modalAltText}
+    />
+  </div>
+</div>
+
+<!-- <ArtModal
+  imageUrl={modalImageUrl}
+  altText={modalAltText}
+  on:close={closeModal}
+/> -->
+<style>
+  /* Style for the modal */
+  .modal {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    z-index: 1000;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    user-select: none;
+    touch-action: pan-x pinch-zoom;
+    pointer-events: none;
+    opacity: 0;
+    visibility: hidden;
+    overflow: auto;
+    background-color: rgba(0, 0, 0, 0.9);
+    transition:
+      opacity 0.5s ease,
+      visibility 0.5s,
+      z-index 0.5s;
+  }
+  .isLoaded {
+    pointer-events: auto;
+    opacity: 1;
+    visibility: visible;
+    z-index: 2000;
+  }
+  #modal-image img {
+    transform: scale(0.95);
+    transition: transform 0.3s ease;
+  }
+  #modal-image img.isLoaded {
+    transform: scale(1);
+  }
+  /* Style for the close button */
+  .close {
+    color: #d83c3c;
+    float: right;
+    font-size: 3rem;
+    font-weight: bold;
+    cursor: pointer;
+    line-height: 0;
+  }
+
+  .close:hover,
+  .close:focus {
+    color: black;
+    text-decoration: none;
+    cursor: pointer;
+  }
+</style>
