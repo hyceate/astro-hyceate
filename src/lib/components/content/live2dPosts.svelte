f27108a2919b3d138df95e59869e1927950d63ca
hyceate <hyceate+github@gmail.com>
2024-04-04 20:12:28 -0400

    artmodal???

--- src/lib/components/content/live2dPosts.svelte
+++ src/lib/components/content/live2dPosts.svelte
@@ -8,7 +8,7 @@
 
 {#each posts as post}
   <li
-    class="live2d-card flex flex-[1_1_15%] w-full m-2 bg-primary border-2 border-darkBorder rounded-lg hover:scale-105 transition-transform ease-in-out motion-reduce:transition-none motion-reduce:hover:transform-none"
+    class="live2d-card flex flex-[1_1_15%] w-full bg-primary border-2 border-darkBorder rounded-lg hover:scale-105 transition-transform ease-in-out motion-reduce:transition-none motion-reduce:hover:transform-none"
   >
     <a href="/projects/{category}/{post.slug}">
       <figure class="w-full max-h-[18rem] overflow-hidden">
