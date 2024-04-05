448ef9845f3f2b451a2e27031a4951ce19c2e8d8
hyceate <hyceate+github@gmail.com>
2024-04-05 18:09:38 -0400

    swup

--- src/lib/components/content/live2dPosts.svelte
+++ src/lib/components/content/live2dPosts.svelte
@@ -8,7 +8,7 @@
 
 {#each posts as post}
   <li
-    class="live2d-card flex flex-[1_1_15%] w-full bg-primary border-2 border-darkBorder rounded-lg hover:scale-105 transition-transform ease-in-out motion-reduce:transition-none motion-reduce:hover:transform-none"
+    class="live2d-card flex flex-[1_1_25%] w-full max-w-[25rem] bg-primary border-2 border-darkBorder rounded-lg hover:scale-[102%] transition-transform ease-in-out motion-reduce:transition-none motion-reduce:hover:transform-none"
   >
     <a href="/projects/{category}/{post.slug}">
       <figure class="w-full max-h-[18rem] overflow-hidden">
@@ -16,8 +16,6 @@
         <Image
           class="flex-[1_1_auto] h-full w-full min-h-[11rem] min-w-[16rem] rounded object-cover object-center"
           src={`https://i.ytimg.com/vi_webp/${post.youtubeEmbed.video.id}/maxresdefault.webp`}
-          loading="lazy"
-          decoding="async"
           layout="fullWidth"
           background="auto"
           alt={post.coverImage.alt}
