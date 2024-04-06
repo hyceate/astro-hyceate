fe676811aec2447f9bb2381b3fd7934a7c1917d6
hyceate <hyceate+github@gmail.com>
2024-04-05 20:44:31 -0400

    add progress bar

--- src/assets/js/transition.js
+++ src/assets/js/transition.js
@@ -2,14 +2,13 @@ import Swup from "swup";
 import SwupA11yPlugin from "@swup/a11y-plugin";
 import SwupHeadPlugin from "@swup/head-plugin";
 import SwupPreloadPlugin from '@swup/preload-plugin';
+import SwupProgressPlugin from '@swup/progress-plugin';
 import { isMenuOpen, isLoading } from "@lib/stores/stores";
 
 const swup = new Swup({
   linkToSelf: 'navigate',
   animationSelector: '[class*="transition-"]',
-  plugins: [new SwupA11yPlugin(), new SwupPreloadPlugin(),
-    new SwupHeadPlugin({
-    }), 
+  plugins: [new SwupA11yPlugin(), new SwupPreloadPlugin(), new SwupProgressPlugin(), new SwupHeadPlugin({}), 
   ],
   containers: ['#swup'],
   timeout: 5_000
