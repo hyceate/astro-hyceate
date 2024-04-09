b796a8dd2bc7f366c82674c9bbc36777fad26824
hyceate <hyceate+github@gmail.com>
2024-04-09 17:24:23 -0400

    a little slow down

--- src/assets/js/transition.js
+++ src/assets/js/transition.js
@@ -1,18 +1,19 @@
-
 import { isMenuOpen, isLoading } from "@lib/stores/stores";
-import { isReady } from "@lib/stores/stores";
 import { get } from "svelte/store";
-
-
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(() => {
+    document.querySelector("#loader").classList.remove("show");
+  }, 550);
+});
 document.addEventListener("astro:before-preparation", () => {
   isLoading.set(true);
   isMenuOpen.set(false);
-  document.querySelector("#loader").classList.remove("hidden");
+  document.querySelector("#loader").classList.add("show");
 });
 document.addEventListener("astro:after-preparation", () => {
   isMenuOpen.set(false);
+  isLoading.set(false);
   setTimeout(()=>{
-    isLoading.set(false);
-    document.querySelector("#loader").classList.add("hidden");
+    document.querySelector("#loader").classList.remove("show");
   }, 550);
 });
